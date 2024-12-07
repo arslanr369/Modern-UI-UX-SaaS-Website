b4a891255b149f2a21896b4aba29178ed84fed72
Arslanr369 <Arslanriaz785@gmail.com>
2024-12-07 15:42:57 +0500

    Update Pricing.jsx

--- src/sections/Pricing.jsx
+++ src/sections/Pricing.jsx
@@ -4,8 +4,12 @@ const Pricing = () => {
   return (
     <section>
       <Element name='pricing'>
-        <div>
-            
+        <div className='container'>
+            <div className='max-w-960 border-2 border-red-500'>
+                <h3 className='h3 max-lg:h4 max-md:h5 z-3 realative max-auto mb-14 max-w '>Flexible pricing for teams of all sizes</h3>
+
+            </div>
+
         </div>
 
       </Element>
