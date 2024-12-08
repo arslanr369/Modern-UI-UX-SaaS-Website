c91ae3049f236a410742269120487b5fadbe8456
Arslanr369 <Arslanriaz785@gmail.com>
2024-12-08 14:45:42 +0500

    Faq Section Done

--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -2,12 +2,10 @@ import { Element } from "react-scroll";
 import { faq } from "../constants/index.jsx";
 import FaqItem from "../components/FaqItem.jsx";
 
-
 const Faq = () => {
-    const halfLength = Math.floor(faq.length / 2);
+  const halfLength = Math.floor(faq.length / 2);
 
-const Faq = () => {
-  return return (
+  return (
     <section>
       <Element name="faq" className="relative">
         <div className="container relative z-2 py-28">
@@ -29,5 +27,24 @@ const Faq = () => {
               <img src="/images/faq-logo.svg" alt="logo" className="size-1/2" />
             </div>
 
+            <div className="relative flex-1 pt-24">
+              {faq.slice(0, halfLength).map((item, index) => (
+                <FaqItem key={item.id} item={item} index={index} />
+              ))}
+            </div>
 
-export default Faq
+            <div className="relative flex-1 lg:pt-24">
+              {faq.slice(halfLength).map((item, index) => (
+                <FaqItem key={item.id} item={item} index={halfLength + index} />
+              ))}
+            </div>
+          </div>
+
+          <div className="faq-lin_after absolute left-[calc(50%-1px)] top-0 -z-1 h-full w-0.5 bg-s2 max-lg:hidden" />
+        </div>
+      </Element>
+    </section>
+  );
+};
+
+export default Faq;
