91cca57596d203cbaf8c439b12a11a9f3b22c3f3
Arslanr369 <Arslanriaz785@gmail.com>
2024-12-09 12:00:45 +0500

    update

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/arslanr369/Modern-UI-UX-SaaS-Website",
-  build: {
-    rollupOptions: {
-      input: "/index.html",
-    },
-  },
-  server: {
-    fs: {
-      strict: false,
-    },
-  },
+  base: './', // Optional: use this if hosting on GitHub Pages
 });
